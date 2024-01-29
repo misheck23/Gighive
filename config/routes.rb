7b46e646686524f6e31bdfe2d59a58555a7021ee
Misheck12 <Misheck1720@gmail.com>
2024-01-29 21:06:21 +0000

    Jan 29, 2024, 11:06 PM

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,44 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define root path
+  root 'dashboard#show'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Dashboard routes
+  get 'dashboard/show'
+  get 'dashboard', to: 'dashboard#show', as: 'dashboard'
+
+  # Devise routes for user authentication with custom controllers for registrations
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
+  # Custom routes within Devise for handling special user registration flows
+  devise_scope :user do
+    get 'users/new_freelancer', to: 'users/registrations#new_freelancer', as: :new_freelancer_registration
+    post 'users/create_freelancer', to: 'users/registrations#create_freelancer', as: :create_freelancer_registration
+    delete 'users/:id', to: 'users/registrations#destroy', as: :admin_destroy_user
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  # Routes for tasks
+  resources :tasks do
+    member do
+      post 'accept'
+      post 'complete'
+      post 'changes'
+      post 'submit_changes'
+
+    end
+
+    # Nested routes for reviews and payments within tasks
+    resources :reviews, only: [:new, :create]
+    resources :payments, only: [:new, :create, :show] do
+      member do
+        patch 'accept', to: 'payments#accept'
+        patch 'reject', to: 'payments#reject'
+      end
+    end
+  end
+
+  # Independent routes for reviews
+  resources :reviews, only: [:show, :edit, :update, :destroy]
+
+  # Any additional routes can be added below
 end
